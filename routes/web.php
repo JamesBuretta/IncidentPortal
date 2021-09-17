8b7fdb8c82058e7f8176f59a82c719e18934d31d
JamesBuretta <jamesburetta39@gmail.com>
2021-09-17 13:01:48 +0300

    - Cleaning unused routes on WEB

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\IncidentsController;
 use App\Http\Controllers\Users\UserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SharedFolder\defaultController;
@@ -31,13 +32,21 @@ use \App\Http\Controllers\CategoriesController;
 |
 */
 
+/*
+ * Authentication page
+ */
 Route::get("/", function () {
     return redirect()->route('auth_login');
 });
 
-//All Middleware Routes
+/*
+* manage token refresh
+*/
 Route::get('/auth/refresh-token', [GuestController::class, 'refreshToken'])->name('refresh_token');
 
+ /*
+  * manage authentication
+  */
 Route::group(['middleware' => ['guest']], function () {
     // Authentication Routes...
     Route::get('/auth/login', [GuestController::class, 'auth_login'])->name('auth_login');
@@ -49,30 +58,38 @@ Route::group(['middleware' => ['guest']], function () {
 
 // Authenticated User Routes...['web', 'auth', 'user']
 Route::group(['middleware' => ['web']], function () {
+    /*
+  * manage dashboard
+  */
     Route::get('/dashboard', [defaultController::class, 'dashboard'])->name('admin_dashboard');
     Route::get('/profile', [defaultController::class, 'profile'])->name('my-profile');
+
+    /*
+   * manage users
+   */
     Route::put('/profile-update/{user_id}', [defaultController::class, 'update_profile'])->name('update_profile');
     Route::put('/user/credentials-update/{user_id}', [defaultController::class, 'update_credentials'])->name('update_credentials');
-
-    //Users Manage Routes
     Route::get('/add-users', [UserController::class, 'add_users'])->name('add-users');
     Route::get('/view-users', [UserController::class, 'view_users'])->name('view-users');
     Route::post('/save-new-user', [UserController::class, 'save_user'])->name('save-new-user');
     Route::put('/user/update-details/{user_id}', [UserController::class, 'update_user_details'])->name('update_user_details');
     Route::put('/user/password/reset/{user_id}', [UserController::class, 'user_password_reset'])->name('user_password_reset');
     Route::delete('/user/remove/{user_id}', [UserController::class, 'remove_user'])->name('remove_user');
-
-    //Manage User Access
     Route::get('/user/access/{user_id}/control', [UserController::class, 'user_access'])->name('user_access');
     Route::put('/update/user/access/{user_id}', [UserController::class, 'update_access'])->name('update-user-access');
+
+
     /**
-     * New Routes
+     * manage companies
      */
-    //Manage User Access
+
     Route::get('/companies', [CompaniesController::class, 'view_companies'])->name('view_companies');
     Route::post('/company/add', [CompaniesController::class, 'save_company'])->name('save_company');
 
-    //vendors
+
+    /**
+     * manage vendors
+     */
     Route::get('/vendors', [VendorsController::class, 'view'])->name('view_vendors');
     Route::get('/vendors/create', [VendorsController::class, 'create'])->name('create_vendor');
     Route::post('/vendors/save', [VendorsController::class, 'save'])->name('save_vendor');
@@ -80,7 +97,9 @@ Route::group(['middleware' => ['web']], function () {
     Route::post('/vendors/update', [VendorsController::class, 'update'])->name('update_vendor');
     Route::post('/vendors/delete', [VendorsController::class, 'delete'])->name('delete_vendor');
 
-    //assets
+    /*
+     * manage assets
+     */
     Route::get('/assets', [AssetsController::class, 'view'])->name('view_assets');
     Route::get('/assets/create', [AssetsController::class, 'create'])->name('create_asset');
     Route::post('/assets/save', [AssetsController::class, 'save'])->name('save_asset');
@@ -88,7 +107,9 @@ Route::group(['middleware' => ['web']], function () {
     Route::post('/assets/update', [AssetsController::class, 'update'])->name('update_asset');
     Route::post('/assets/delete', [AssetsController::class, 'delete'])->name('delete_asset');
 
-    //companies
+    /*
+    * manage companies
+    */
     Route::get('/companies', [CompaniesController::class, 'view'])->name('view_companies');
     Route::get('/companies/create', [CompaniesController::class, 'create'])->name('create_company');
     Route::post('/companies/save', [CompaniesController::class, 'save'])->name('save_company');
@@ -96,7 +117,9 @@ Route::group(['middleware' => ['web']], function () {
     Route::post('/companies/update', [CompaniesController::class, 'update'])->name('update_company');
     Route::post('/companies/delete', [CompaniesController::class, 'delete'])->name('delete_company');
 
-    //stations
+    /*
+   * manage stations
+   */
     Route::get('/stations', [StationsController::class, 'view'])->name('view_stations');
     Route::get('/stations/create', [StationsController::class, 'create'])->name('create_station');
     Route::post('/stations/save', [StationsController::class, 'save'])->name('save_station');
@@ -104,14 +127,18 @@ Route::group(['middleware' => ['web']], function () {
     Route::post('/stations/update', [StationsController::class, 'update'])->name('update_station');
     Route::post('/stations/delete', [StationsController::class, 'delete'])->name('delete_station');
 
-    //inventory
+    /*
+   * manage inventories
+   */
     Route::get('/inventory', [InventoryController::class, 'view'])->name('view_allocations');
     Route::get('/inventory/allocate', [InventoryController::class, 'allocate_view'])->name('allocate_assets');
     Route::post('/inventory/allocate', [InventoryController::class, 'allocate'])->name('allocate_asset');
     Route::post('/inventory/disallocate', [InventoryController::class, 'disallocate'])->name('disallocate_asset');
     Route::post('/inventory/delete', [InventoryController::class, 'delete'])->name('delete_allocation');
 
-    //categories
+    /*
+   * manage categories
+   */
     Route::get('/categories', [CategoriesController::class, 'view'])->name('view_categories');
     Route::get('/categories/create', [CategoriesController::class, 'create'])->name('create_category');
     Route::post('/categories/save', [CategoriesController::class, 'save'])->name('save_category');
@@ -119,43 +146,30 @@ Route::group(['middleware' => ['web']], function () {
     Route::post('/categories/update', [CategoriesController::class, 'update'])->name('update_category');
     Route::post('/categories/delete', [CategoriesController::class, 'delete'])->name('delete_category');
 
-    //Payment's
-    Route::get('/payments', [PaymentController::class, 'payments_information'])->name('payments_info');
-    Route::get('/payments/history/graph', [PaymentController::class, 'load_payment_graph_data'])->name('load_payment_graph_data');
-    Route::get('/download/invoice/{invoiceID}', [PaymentController::class, 'download_invoice'])->name('download_invoice');
 
-    //Role Management
+    /*
+   * manage assets
+   */
     Route::get('view_roles/{id}', [RoleController::class, 'edit'])->name('view_roles');
-
     Route::get('view_roles', [RoleController::class, 'index'])->name('view_roles');
-
     Route::get('delete_role/{id}', [RoleController::class, 'destroy'])->name('delete_role');;
-
     Route::put('update_role/{id}', [RoleController::class, 'update'])->name('update_role');
-
     Route::get('create_role', [RoleController::class, 'create'])->name('create_role');
-
     Route::post('create_role', [RoleController::class, 'store'])->name('store_role');
 
-    //Incident Management
-    Route::get('view_incidents/{id}', [\App\Http\Controllers\IncidentsController::class, 'edit'])->name('view_incidents');
-
-    Route::get('view_incidents', [\App\Http\Controllers\IncidentsController::class, 'index'])->name('view_incidents');
-
-    Route::get('delete_incident/{id}', [\App\Http\Controllers\IncidentsController::class, 'destroy'])->name('delete_incident');;
-
-    Route::put('update_incident/{id}', [\App\Http\Controllers\IncidentsController::class, 'update'])->name('update_incident');
-
-    Route::get('create_incident', [\App\Http\Controllers\IncidentsController::class, 'index'])->name('create_incident');
-
-    Route::post('create_incident', [\App\Http\Controllers\IncidentsController::class, 'store'])->name('store_incident');
-
-    Route::post('close', [\App\Http\Controllers\IncidentsController::class, 'close'])->name('close');
-
-    Route::post('cancel', [\App\Http\Controllers\IncidentsController::class, 'cancel'])->name('cancel');
-
-    Route::get('reports', [\App\Http\Controllers\IncidentsController::class, 'report'])->name('reports');
-    Route::post('reports_filtered', [\App\Http\Controllers\IncidentsController::class, 'reportfiltered'])->name('reports_filtered');
+    /*
+   * manage incidents
+   */
+    Route::get('view_incidents/{id}', [IncidentsController::class, 'edit'])->name('view_incidents');
+    Route::get('view_incidents', [IncidentsController::class, 'index'])->name('view_incidents');
+    Route::get('delete_incident/{id}', [IncidentsController::class, 'destroy'])->name('delete_incident');;
+    Route::put('update_incident/{id}', [IncidentsController::class, 'update'])->name('update_incident');
+    Route::get('create_incident', [IncidentsController::class, 'index'])->name('create_incident');
+    Route::post('create_incident', [IncidentsController::class, 'store'])->name('store_incident');
+    Route::post('close', [IncidentsController::class, 'close'])->name('close');
+    Route::post('cancel', [IncidentsController::class, 'cancel'])->name('cancel');
+    Route::get('reports', [IncidentsController::class, 'report'])->name('reports');
+    Route::post('reports_filtered', [IncidentsController::class, 'reportfiltered'])->name('reports_filtered');
 });
 
 Route::group(['middleware' => ['auth']], function () {
