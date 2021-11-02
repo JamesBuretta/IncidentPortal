c013777243e3976d98be41ad68c033cc6e7cd984
JamesBuretta <jamesburetta39@gmail.com>
2021-11-02 13:08:34 +0300

    - SLA update

---
+++ app/Http/Controllers/SLA/SLAController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\SLA;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SLAController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function edit()
+    {
+
+    }
+
+    public function update(Request $request)
+    {
+
+    }
+
+    public function add()
+    {
+
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+}
